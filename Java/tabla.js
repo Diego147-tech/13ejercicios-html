66e6b743999c2846780b2a7b075c828ccdf603d2
Diego147-tech <109808175+Diego147-tech@users.noreply.github.com>
2022-07-29 11:10:04 -0500

    requerid

--- Java/tabla.js
+++ Java/tabla.js
@@ -1,9 +1,9 @@
-function label(){
-   
-    var a= parseInt(document.tablon.n1.value);
-    for(var i=0; 1<=10; i++)
-    {
-        document.write(+a + 'x' +i+ '=' +a*i +'<br>'); 
-        
-    }
+function tablas(){
+    var a = parseFloat(document.Form1.a1.value);
+
+    while(a<1|a>50); //condicion
+    for( var i = 1; i<=10; i++)// operacion
+{
+document.write(+a + 'x' +i+ '=' +a*i +'<br>'); // MUESTRA EN PANTALLA
+}
 }
